9ce7e8449c3ff9729289de003ca6f18d970d11b7
Luciano Lorenzo <lucianoalorenzo1@gmail.com>
2023-11-16 19:09:26 -0300

    adding a hover effect to walls

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ interface Wall {
 
 function App() {
 	const [walls, setWalls] = useState<Wall[][]>(matrix(9, 9));
+	const [hoveredWall, setHoveredWall] = useState<{pos: PawnPos, wall: Wall} | null>(null);
 	const [whitePawnPos, setWhitePawnPos] = useState<PawnPos>({ x: 0, y: 4, });
 	const [blackPawnPos, setBlackPawnPos] = useState<PawnPos>({ x: 8, y: 4, });
 	const [turn, setTurn] = useState<Boolean>(true);
@@ -59,10 +60,30 @@ function App() {
 		});
 	};
 
+	const handleHover = (e: MouseEvent<HTMLDivElement>) => {
+		const target = e.target as HTMLDivElement;
+		const id = target.id;
+		let row = +(target.getAttribute("data-row") || 1);
+		let col = +(target.getAttribute("data-col") || 0);
+
+		if (id == "horizontal-wall" || id == "intersection") {
+			let r = pickHorizontalWall(row, col, walls)
+			if (!r) return null
+			setHoveredWall({pos: {x: r.row, y: r.col}, wall: {row: 0, col: 1}})
+		} else if (id == "vertical-wall") {
+			let r = pickVerticalWall(row, col, walls)
+			if (!r) return null
+			setHoveredWall({pos: {x: r.row, y: r.col}, wall: {row: 1, col: 0}})
+		} else {
+			return setHoveredWall(null)
+		}
+	}
+
 	return (
 		<div
 			onClick={(e) => handleClick(e)}
-			className="w-screen h-screen flex justify-center items-center bg-red-100"
+			onMouseOver={(e) => handleHover(e)}
+			className="w-screen h-screen flex justify-center items-center"
 		>
 			{walls.map((f, col) => {
 				return (
@@ -76,12 +97,13 @@ function App() {
 											col={col}
 											state={
 												whitePawnPos.x == row && whitePawnPos.y == col ? 1 : 0 ||
-												blackPawnPos.x == row && blackPawnPos.y == col ? 2 : 0
+													blackPawnPos.x == row && blackPawnPos.y == col ? 2 : 0
 											}
 										/>
 										{row < 8 ? (
 											<Wall
 												state={c.row}
+												hovered={hoveredWall && hoveredWall.wall.col &&  (hoveredWall.pos.x == row && hoveredWall.pos.y == col || hoveredWall.pos.x == row && hoveredWall.pos.y == col-1) ? true : false}
 												row={row}
 												col={col}
 												horizontal={true}
@@ -98,9 +120,9 @@ function App() {
 								{f.map((c, row) => {
 									return (
 										<Fragment key={`wall-${row}-${col}`}>
-											<Wall state={c.col} row={row} col={col} />
+											<Wall state={c.col} row={row} col={col} hovered={hoveredWall && hoveredWall.wall.row && (hoveredWall.pos.x == row && hoveredWall.pos.y == col || hoveredWall.pos.x == row-1 && hoveredWall.pos.y == col) ? true : false} />
 											{row < 8 ? (
-												<Intersection row={row} col={col} state={c} />
+												<Intersection row={row} col={col} state={c} hovered={hoveredWall && hoveredWall.pos.x == row && hoveredWall.pos.y == col ? hoveredWall.wall : undefined} />
 											) : (
 												""
 											)}
@@ -147,13 +169,15 @@ const Wall = ({
 	row,
 	col,
 	horizontal = false,
+	hovered,
 }: {
 	state: number;
 	row: number;
 	col: number;
 	horizontal?: boolean;
+	hovered: boolean;
 }) => {
-	const color = state == 0 ? "bg-white-500" : "bg-yellow-500";
+	const color = state != 0 ?  "bg-yellow-500" : (hovered ? "bg-yellow-300" : "bg-white-500");
 	const size = horizontal ? "w-12 h-4" : "h-12 w-4";
 
 	return (
@@ -170,16 +194,18 @@ const Intersection = ({
 	row,
 	col,
 	state,
+	hovered,
 }: {
 	row: number;
 	col: number;
 	state: { row: number; col: number };
+	hovered?: { row: number; col: number };
 }) => {
+	console.log(hovered)
 	return (
 		<div
 			id="intersection"
-			className={`${state.row == 1 || state.col == 1 ? "bg-yellow-500" : "bg-white-500"
-				} w-4 h-4`}
+			className={`${state.row == 1 || state.col == 1 ? "bg-yellow-500" : (hovered && (hovered.row == 1 || hovered.col == 1) ?  "bg-yellow-300" : "bg-white-500") } w-4 h-4`}
 			data-row={row}
 			data-col={col}
 		/>
@@ -261,30 +287,10 @@ const dfs = (pos: PawnPos, end: number, walls: Wall[][]) => {
 
 const CheckValidWalls = (id: string, row: number, col: number, walls: Wall[][]): Wall[][] | null => {
 	if (id == "horizontal-wall" || id == "intersection") {
-		if (row > 7) row = 7;
-		if (col > 7) col = 7;
-
-		if (walls[col + 1][row].row == 1) {
-			if (col >= 1 && walls[col - 1][row].col == 0) {
-				col -= 1;
-			} else {
-				return null;
-			}
-		}
-
-		if (walls[col][row].row != 0) {
-			return null;
-		}
-
-		if (walls[col][row].col == 1) {
-			if (
-				col < 1 ||
-				walls[col - 1][row].col == 1 ||
-				walls[col - 1][row].row != 0
-			)
-				return null;
-			col -= 1;
-		}
+		let r = pickHorizontalWall(row, col, walls)
+		if (!r) return null
+		row = r.row
+		col = r.col
 
 		const copy: Wall[][] = structuredClone(walls);
 		copy[col][row] = { row: 1, col: walls[col][row].col };
@@ -293,18 +299,10 @@ const CheckValidWalls = (id: string, row: number, col: number, walls: Wall[][]):
 	}
 
 	if (id == "vertical-wall") {
-		if (row > 7) row = 7;
-		if (col > 7) col = 7;
-
-		if (walls[col][row + 1].col == 1) {
-			if (row >= 1 && walls[col][row - 1].row == 0) {
-				row -= 1;
-			} else {
-				return null;
-			}
-		}
-
-		if (walls[col][row].row == 1 || walls[col][row].col != 0) return null;
+		let r = pickVerticalWall(row, col, walls)
+		if (!r) return null
+		row = r.row
+		col = r.col
 
 		const copy = structuredClone(walls);
 		copy[col][row] = { row: walls[col][row].row, col: 1 };
@@ -313,5 +311,53 @@ const CheckValidWalls = (id: string, row: number, col: number, walls: Wall[][]):
 	}
 	return null
 }
+
+const pickVerticalWall = (row: number, col: number, walls: Wall[][]): { row: number, col: number } | null => {
+	if (row > 7) row = 7;
+	if (col > 7) col = 7;
+
+	if (walls[col][row + 1].col == 1) {
+		if (row >= 1 && walls[col][row - 1].row == 0) {
+			row -= 1;
+		} else {
+			return null;
+		}
+	}
+
+	if (walls[col][row].row == 1 || walls[col][row].col != 0) return null;
+
+	return { row, col }
+}
+
+const pickHorizontalWall = (row: number, col: number, walls: Wall[][]): { row: number, col: number } | null => {
+	if (row > 7) row = 7;
+	if (col > 7) col = 7;
+
+	if (walls[col + 1][row].row == 1) {
+		if (col >= 1 && walls[col - 1][row].col == 0) {
+			col -= 1;
+		} else {
+			return null;
+		}
+	}
+
+	if (walls[col][row].row != 0) {
+		return null;
+	}
+
+	if (walls[col][row].col == 1) {
+		if (
+			col < 1 ||
+			walls[col - 1][row].col == 1 ||
+			walls[col - 1][row].row != 0
+		)
+			return null;
+		col -= 1;
+	}
+
+	return { row, col }
+}
+
+
 
 export default App;
